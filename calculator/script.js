8816d8c175a7649c807de86457cc4106dadd0e8a
Wambita Fana <wambitafana@gmail.com>
2019-02-12 16:07:53 +0300

    added extra functions and title

--- calculator/script.js
+++ calculator/script.js
@@ -1,13 +1,20 @@
 var add = function(number1, number2) {
-    return number1 -number2;
+    return number1 + number2;
     };
     add();
-   // var subtract = function(number1, number2){
-    //return number1 - number2;
-   // };
-    //alert(subtract(10,5));
+    var subtract = function(number1, number2){
+    return number1 - number2;
+};
+var multiply =function(number1, number2) {
+    return number1 * number2;
+};
+var divide = function(number1, number2) {
+    return number1/ number2;
+};
     var number1 = parseInt(prompt("enter a number:"));
     alert(number1);
     var number2 =parseInt(prompt("enter another number :"));
-    alert(add(number1, number2));
+    alert(number2);
+    var result = divide(number1, number2);
+    alert (result);
     
