92eb4da051a9cff03fceb6a9554a6a61795999f8
Wambita Fana <wambitafana@gmail.com>
2019-02-12 15:47:35 +0300

    add script.js

---
+++ calculator/script.js
@@ -0,0 +1,13 @@
+var add = function(number1, number2) {
+    return number1 -number2;
+    };
+    add();
+   // var subtract = function(number1, number2){
+    //return number1 - number2;
+   // };
+    //alert(subtract(10,5));
+    var number1 = parseInt(prompt("enter a number:"));
+    alert(number1);
+    var number2 =parseInt(prompt("enter another number :"));
+    alert(add(number1, number2));
+    
